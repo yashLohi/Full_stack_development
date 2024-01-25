b9c10958c3e5dce967ab06b4bdfbca8b2a09e7eb
Yash Lohi <ylohi31@gmail.com>
2024-01-25 21:08:49 +0530

    Learn basics

---
+++ JavaScript/01.Basics/app.js
@@ -0,0 +1,103 @@
+// alert("something to show");
+
+// Declared variable
+// let namee;
+
+// // Assign value
+// let banana;
+// banana = "banana"
+
+// console.log(banana);
+
+
+
+// let name = "Yash Lohi"
+// console.log(name);
+
+// let name = "Yash Lohi";
+// let whatDoYouWannaBecomeInYourLife = "programmer";
+// let gender = "male";
+// let twitterHandle = "YashLohi3";
+// console.log(name);
+// console.log(whatDoYouWannaBecomeInYourLife);
+// console.log(gender);
+// console.log(twitterHandle);
+
+
+// let firstFavNum = 13;
+// let secondFavNum = 12;
+// console.log(firstFavNum + secondFavNum);
+// console.log(firstFavNum - secondFavNum);
+// console.log(firstFavNum * secondFavNum);
+// console.log(firstFavNum / secondFavNum);
+// console.log(firstFavNum % secondFavNum);
+// console.log(firstFavNum ** secondFavNum);
+
+// let number = 10;
+// console.log(number + undefined)  // NaN
+
+// let isJsProrammingLanguage = true;
+// let isJsHard = false;
+// let favNumb = 13;
+// console.log(isJsProrammingLanguage);
+// console.log(isJsHard);
+// console.log(favNumb + undefined);
+
+
+// let firstFavNumb = 21
+// let secondFavNumb = 10
+// console.log(firstFavNumb > secondFavNumb);
+// console.log(firstFavNumb < secondFavNumb);
+// console.log(firstFavNumb >= secondFavNumb);
+// console.log(firstFavNumb <= secondFavNumb);
+// console.log(firstFavNumb === secondFavNumb);
+// console.log(firstFavNumb == secondFavNumb);
+// console.log(firstFavNumb !== secondFavNumb);
+
+
+// let firstName = "Yash";
+// let secondName = "Lohi";
+// let fullName = firstName + " " + secondName;
+// let fullName = firstName.concat(secondName);
+// console.log(fullName);
+
+// firstName += " something else";
+// console.log(firstName);
+
+
+// console.log(firstName.slice(0, 7));
+
+// console.log(firstName.split(" ").join(" "));
+
+// let favActorFirstName = "NoOne";
+// let favActorLastName = " NOONE";
+
+// let fullName = favActorFirstName.concat(favActorLastName)
+// console.log(fullName);
+
+// let uppercase = fullName.toUpperCase();
+// console.log(uppercase);
+
+// let message = `My favorite Actor is ${uppercase} & say something about your fav actor`
+// console.log(message);
+
+// message += " Silicon Valley"
+// console.log(message);
+
+
+let money = "50";
+console.log(typeof money)
+
+// -Covert string to number
+
+money = parseInt(money);
+// money = +money
+console.log(typeof money)
+
+
+// convet number to String
+let cash = 50
+console.log(typeof cash)
+
+cash = cash.toString();
+console.log(typeof cash)
